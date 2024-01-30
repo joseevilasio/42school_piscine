5936bffe747a24d257a134530c0484b5d138949c
joseevilasio <jose_evilasio@outlook.com>
2024-01-30 19:40:45 +0000

    added ex00 c11

---
+++ c11/ex00/ft_foreach.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 19:17:58 by josejunior        #+#    #+#             */
+/*   Updated: 2024/01/30 19:38:55 by josejunior       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		write(1, "147483648", 9);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+		ft_putchar(nb % 10 + '0');
+}
+
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		f(tab[i]);
+		ft_putchar(' ');
+		i++;
+	}
+}
+
+int	main(void)
+{
+	int tab[6]= {23, 12, 12, 1, 4, 9};
+	int length;
+
+	length = 6;
+	ft_foreach(tab, length, &ft_putnbr);
+	return (0);
+}
