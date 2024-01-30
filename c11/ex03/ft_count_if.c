9641c93f866512dd812a33e050d4df471ce8c0f0
joseevilasio <jose_evilasio@outlook.com>
2024-01-30 21:47:03 +0000

    added ex03 c11

---
+++ c11/ex03/ft_count_if.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 21:13:30 by josejunior        #+#    #+#             */
+/*   Updated: 2024/01/30 21:42:53 by josejunior       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_count_if(char **tab, int length, int (*f)(char*))
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (i < length)
+	{
+		if (f(tab[i]) != 0)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+int	main(void)
+{
+	char	*tab[6] = {"banana", "BISCOITO", "vitamina", "LEITE"};
+	int		length;
+
+	length = 4;
+	printf("%d", ft_count_if(tab, length, &ft_str_is_uppercase));
+	return (0);
+}
